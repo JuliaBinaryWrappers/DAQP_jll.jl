adf91f1026fb1615aa9952bf097d3821104963ad
jlbuild <juliabuildbot@gmail.com>
2026-03-18 08:14:05 +0000

    DAQP_jll build 0.8.0+0

--- src/DAQP_jll.jl
+++ src/DAQP_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("DAQP")
-JLLWrappers.@generate_main_file("DAQP", UUID("5c51c916-43bf-57fe-9d62-13064ebbf40d"))
+JLLWrappers.@generate_main_file("DAQP", Base.UUID("5c51c916-43bf-57fe-9d62-13064ebbf40d"))
 end  # module DAQP_jll
